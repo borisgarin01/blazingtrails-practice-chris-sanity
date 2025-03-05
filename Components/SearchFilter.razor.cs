dbec93b42b4dd116b2d0b76e06f8021db5b21a2b
@boris <boris_it@fortuna99.ru>
2025-03-05 10:41:22 +0300

    Фильтрация по названию и длине тропы добавлены

---
+++ Components/SearchFilter.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWasm1.Components;
+
+public partial class SearchFilter
+{
+    private int maxLength;
+
+    [Parameter, EditorRequired]
+    public string SearchTerm { get; set; } = default!;
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    private void FilterSearchResults() => NavigationManager.NavigateTo($"/search/{SearchTerm}/maxlength/{maxLength}");
+
+    private void ClearSearchFilter()
+    {
+        maxLength = 0;
+        NavigationManager.NavigateTo($"/search/{SearchTerm}");
+    }
+}
--- Components/SearchPage.razor.cs
+++ Components/SearchPage.razor.cs
@@ -12,8 +12,13 @@ public partial class SearchPage
     [Parameter]
     public string SearchTerm { get; set; } = default!;
 
+    [Parameter]
+    public int? MaxLength { get; set; }
+
     private IEnumerable<Trail>? searchResults;
     private Trail? selectedTrail;
+
+    private IEnumerable<Trail> cachedSearchResults = Array.Empty<Trail>();
 
     protected override async Task OnInitializedAsync()
     {
@@ -21,10 +26,24 @@ public partial class SearchPage
         {
             var allTrails = await HttpClient.GetFromJsonAsync<IEnumerable<Trail>>("trails/trail-data.json");
             searchResults = allTrails!.Where(x => x.Name.Contains(SearchTerm, StringComparison.CurrentCultureIgnoreCase) || x.Location.Contains(SearchTerm, StringComparison.CurrentCultureIgnoreCase));
+            cachedSearchResults = searchResults;
         }
         catch (HttpRequestException ex)
         {
             Console.WriteLine($"There was a problem loading trail data: {ex.Message}");
+        }
+    }
+
+    protected override void OnParametersSet()
+    {
+        if (cachedSearchResults.Any() && MaxLength.HasValue)
+        {
+            searchResults = cachedSearchResults
+                .Where(x => x.Length <= MaxLength.Value);
+        }
+        else if (cachedSearchResults.Any() && MaxLength is null)
+        {
+            searchResults = cachedSearchResults;
         }
     }
 
